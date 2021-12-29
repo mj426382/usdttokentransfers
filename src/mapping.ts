7784e82186977f51d74105856261a50b8162282c
mateusz <mistrzumati@spoko.pl>
2021-12-29 11:05:24 +0100

    Migrate to 0.0.2

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,10 +1,36 @@
 import {
-  Transfer
+  Issue,
+  Redeem,
+  Deprecate,
+  Params,
+  DestroyedBlackFunds,
+  AddedBlackList,
+  RemovedBlackList,
+  Approval,
+  Transfer,
+  Pause,
+  Unpause
 } from "../generated/UsdtToken/UsdtToken"
 import { TransferEvent } from "../generated/schema"
 
 const lender = '0x16d02Dc67EB237C387023339356b25d1D54b0922'
 const lender2 = '0xa606dd423dF7dFb65Efe14ab66f5fDEBf62FF583'
+
+export function handleIssue(event: Issue): void {}
+
+export function handleRedeem(event: Redeem): void {}
+
+export function handleDeprecate(event: Deprecate): void {}
+
+export function handleParams(event: Params): void {}
+
+export function handleDestroyedBlackFunds(event: DestroyedBlackFunds): void {}
+
+export function handleAddedBlackList(event: AddedBlackList): void {}
+
+export function handleRemovedBlackList(event: RemovedBlackList): void {}
+
+export function handleApproval(event: Approval): void {}
 
 export function handleTransfer(event: Transfer): void {
 
@@ -21,3 +47,7 @@ export function handleTransfer(event: Transfer): void {
 
   entity.save()
 }
+
+export function handlePause(event: Pause): void {}
+
+export function handleUnpause(event: Unpause): void {}
